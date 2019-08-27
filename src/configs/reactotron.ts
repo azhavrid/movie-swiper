e3ad6b9d1f5649e3ea9637627c67d68237912d37
Alexey Zhavrid <alexzvrd@gmail.com>
2019-08-27 15:03:36 +0200

    Add config files

---
+++ src/configs/reactotron.ts
@@ -0,0 +1,28 @@
+import Reactotron from 'reactotron-react-native';
+import sagaPlugin from 'reactotron-redux-saga';
+import { reactotronRedux } from 'reactotron-redux';
+
+export const reactotron = Reactotron.configure({ name: 'Movie Swiper' })
+  // TODO: Check which plugins to use
+  // .use(
+  //   trackGlobalErrors({
+  //     // ignore all error frames from react-native (for example)
+  //     veto: frame => frame.fileName.indexOf('/node_modules/react-native/') >= 0,
+  //   }),
+  // )
+  // .use(networking())
+  .use(sagaPlugin({}))
+  .use(reactotronRedux({}))
+  .useReactNative({})
+  .connect();
+
+// Clear Reactotron on app start
+Reactotron.clear && Reactotron.clear();
+
+// Add Reactotron to console and declare global Console
+console.tron = reactotron;
+declare global {
+  interface Console {
+    tron: typeof reactotron;
+  }
+}
