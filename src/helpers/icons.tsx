fc954f354f76e509b27ffd18c7d54b7a9425e4e7
Alexey Zhavrid <alexzvrd@gmail.com>
2019-09-13 15:13:10 +0200

    Refactor icons helper

---
+++ src/helpers/icons.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import IconEntypo from 'react-native-vector-icons/Entypo';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
+import IconEvilIcons from 'react-native-vector-icons/EvilIcons';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { theme } from '../theme';
+import { ViewStyle } from 'react-native';
+
+/* ------------- Types ------------- */
+interface TintColorParam {
+  tintColor: string;
+}
+
+interface StyleParam {
+  style: ViewStyle;
+}
+
+/* ------------- Localize ------------- */
+const { smallIconSize, iconSize, hugeIconSize } = theme.specifications;
+const { primary } = theme.colors;
+const { lightest, light, darkest } = theme.gray;
+const { tiny } = theme.spacing;
+
+/* ------------- Icons ------------- */
+// --- MovieDetailsButtons ---
+export const getAddToWatchlistIcon = ({ isInWatchlist }: { isInWatchlist: boolean }) => (
+  <IconMaterialIcons
+    name={isInWatchlist ? 'playlist-add-check' : 'playlist-add'}
+    color={isInWatchlist ? primary : lightest}
+    size={iconSize}
+  />
+);
+
+export const getAddToFavoritesIcon = ({ isInFavorites }: { isInFavorites: boolean }) => (
+  <IconMaterialIcons
+    name={isInFavorites ? 'favorite' : 'favorite-border'}
+    color={isInFavorites ? primary : lightest}
+    size={iconSize}
+  />
+);
+
+export const getOpenImdbIcon = ({ disabled }: { disabled: boolean }) => (
+  <IconMaterialCommunityIcons name="movie-roll" color={disabled ? light : lightest} size={iconSize} />
+);
+
+// --- MovieSearchResults ---
+export const getEmptySearchIcon = () => <IconFeather name="alert-circle" color={lightest} size={hugeIconSize} />;
+
+export const getInitialSearchIcon = () => <IconEvilIcons name="search" color={lightest} size={hugeIconSize} />;
+
+// --- GuestInfo ---
+export const getGuestInfoIcon = () => <IconFeather name="user" color={lightest} size={hugeIconSize} />;
+
+// --- SearchInput ---
+export const getSearchInputBackIcon = ({ style }: StyleParam) => (
+  <IconEntypo name="chevron-thin-left" size={smallIconSize} color={darkest} style={style} />
+);
+
+export const getSearchInputLabelIcon = () => (
+  <IconFeather name="search" color={darkest} size={smallIconSize * 1.1} style={{ paddingHorizontal: tiny }} />
+);
+
+export const getSearchInputCloseIcon = () => <IconAntDesign name="close" color={darkest} size={smallIconSize * 1.2} />;
+
+// --- MovieList ---
+export const getMovieListEmptyIcon = () => <IconAntDesign name="smileo" color={lightest} size={hugeIconSize} />;
+
+// --- Library ---
+export const getLibrarySettingsIcon = () => (
+  <IconFontAwesome name="cog" color={lightest} size={iconSize * 0.9} style={{ padding: tiny }} />
+);
+
+export const getLibraryWatchlistIcon = () => (
+  <IconMaterialIcons name="watch-later" color={light} size={iconSize * 0.8} />
+);
+
+export const getLibraryFavoriteIcon = () => (
+  <IconMaterialCommunityIcons name="heart-multiple" color={light} size={iconSize * 0.8} />
+);
+
+// --- Header ---
+export const getHeaderBackIcon = () => (
+  <IconEntypo name="chevron-left" color={lightest} size={iconSize} style={{ padding: theme.spacing.tiny }} />
+);
+
+// --- Routes ---
+export const getNavbarBrowseIcon = ({ tintColor }: TintColorParam) => (
+  <IconEntypo name="home" color={tintColor} size={iconSize * 0.9} />
+);
+
+export const getNavbarExploreIcon = ({ tintColor }: TintColorParam) => (
+  <IconMaterialIcons name="photo-library" color={tintColor} size={iconSize * 0.9} />
+);
+
+export const getNavbarLibraryIcon = ({ tintColor }: TintColorParam) => (
+  <IconEntypo name="folder-video" color={tintColor} size={iconSize * 0.85} />
+);
