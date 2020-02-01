6d584446031229ce78ea927fe0efbc50e477c3e2
Alexey Zhavrid <alexzvrd@gmail.com>
2020-02-01 13:11:26 +0300

    Add route names

---
+++ src/routes/routeNames.ts
@@ -0,0 +1,20 @@
+export const routeNames = {
+  Splash: 'Splash',
+  AuthStack: 'AuthStack',
+  HomeStack: 'HomeStack',
+
+  AuthWelcome: 'AuthWelcome',
+  AuthLogin: 'AuthLogin',
+
+  Settings: 'Settings',
+  SectionListScreen: 'SectionListScreen',
+  MovieDetailsScreen: 'MovieDetailsScreen',
+
+  BottomTabs: 'BottomTabs',
+};
+
+export const tabNames = {
+  browse: 'Browse',
+  explore: 'Explore',
+  library: 'Library',
+};
