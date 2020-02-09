900f8025601ade93409b15c69f1a8657e932c600
Alexey Zhavrid <alexzvrd@gmail.com>
2020-02-09 18:32:15 +0100

    Change redux store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+import { persistStore } from 'redux-persist';
 
+import { afterRehydrate } from './rehydrate/actions';
 import { reactotron } from '../configs/reactotron/reactotron';
 import { rootReducer } from './reducer';
 import { rootSaga } from './saga';
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
+import StoreService from './StoreService';
 
 const middleware = [];
 let sagaMonitor;
@@ -27,12 +23,11 @@ if (__DEV__) {
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 middleware.push(applyMiddleware(sagaMiddleware));
 
-// Persisted
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// Redux
+export const store = createStore(rootReducer, {}, compose(...middleware));
+export const persistor = persistStore(store, {}, () => store.dispatch(afterRehydrate()));
 
-// Create store
-export const store = createStore(persistedReducer, {}, compose(...middleware));
-export const persistor = persistStore(store);
+StoreService.setStoreReference(store);
 
-// Run saga middleware after store creation
+// Run saga middleware; Must be called after store is created
 sagaMiddleware && sagaMiddleware.run(rootSaga);
