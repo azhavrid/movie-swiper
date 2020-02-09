48bf84bdfe1aead13a4216b9f0d2a61b62bd2d33
Alexey Zhavrid <alexzvrd@gmail.com>
2020-02-09 18:32:04 +0100

    Fix RootAction type

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -3,13 +3,12 @@ import { AuthAction } from './auth/actions';
 import { ExploreAction } from './explore/actions';
 import { MoviesAction } from './movies/actions';
 import { NetworkAction } from './network/actions';
-import { RehydrateAction } from 'redux-persist';
+import { RehydrateAction } from './rehydrate/actions';
 import { SearchAction } from './search/actions';
 import { SectionAction } from './sections/actions';
 
 /* ------------- State ------------- */
 export type RootState = ReturnType<typeof persistedReducer>;
-
 export type RootAction =
   | AuthAction
   | ExploreAction
@@ -20,11 +19,6 @@ export type RootAction =
   | SectionAction;
 
 /* ------------- Other ------------- */
-export interface ResponseError {
-  message: string;
-  raisedAt: number;
-}
-
 export interface WithCallbacks {
   onSuccess?: () => void;
   onError?: () => void;
