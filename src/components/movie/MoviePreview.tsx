2ec8cee10b9d70a8b867c996c3f0845b0f03905b
Alexey Zhavrid <alexzvrd@gmail.com>
2020-02-21 16:16:27 +0100

    Refactor movie preview

--- src/components/movie/MoviePreview.tsx
+++ src/components/movie/MoviePreview.tsx
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
 });
 
 const makeMapStateToProps = (state: RootState, props: PropsWithoutRedux) => {
-  const movieId = props.movieId;
+  const { movieId } = props;
   const movieSelector = getMovieSelectorById(movieId);
 
   return (state: RootState) => ({
