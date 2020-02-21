dda0a238b507ea8d9fe48757892d190fcd50f2bb
Alexey Zhavrid <alexzvrd@gmail.com>
2020-02-21 20:57:06 +0100

    Momoize explore movie id selector

--- src/redux/movies/selectors.ts
+++ src/redux/movies/selectors.ts
@@ -1,3 +1,4 @@
+import memoizeOne from 'memoize-one';
 import { createSelector } from 'reselect';
 import { RootState } from '../types';
 import { MovieId } from './types';
@@ -5,3 +6,7 @@ import { MovieId } from './types';
 export const moviesSelector = (state: RootState) => state.movies;
 
 export const getMovieSelectorById = (movieId: MovieId) => createSelector(moviesSelector, movies => movies[movieId]);
+
+export const getMoviesSelectorByIds = memoizeOne((movieIds: MovieId[]) =>
+  createSelector(moviesSelector, movies => movieIds.map(movieId => movies[movieId])),
+);
